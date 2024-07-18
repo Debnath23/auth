471c2fd5769d05869c7701753e777c93c023b175
Debnath Mahapatra <128223356+Debnath23@users.noreply.github.com>
2024-07-18 13:49:01 +0530

    Add files via upload

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { UserInterface } from '../user.interface';
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { FooterComponent } from "../footer/footer.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink, NavBarComponent, FooterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent  {
+  // authService = inject(AuthService); implements OnInit
+  // http = inject(HttpClient);
+
+  // ngOnInit(): void {
+  //   this.http.get<{user: UserInterface}>('https://api.realworld.io/api/user')
+  //   .subscribe({
+  //     next: (response) => {
+  //       console.log('response', response);
+  //       this.authService.currentUserSig.set(response.user);
+  //     }, error: () => {
+  //       this.authService.currentUserSig.set(null);
+  //     }})
+  // }
+
+  // logout(){
+  //   console.log("logout");
+  //   localStorage.setItem('token', '');
+  //   this.authService.currentUserSig.set(null);
+  // }
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,63 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { UserInterface } from '../user.interface';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  fb = inject(FormBuilder);
+  http = inject(HttpClient);
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  form = this.fb.nonNullable.group({
+    email: ['', { validators: [Validators.required, Validators.email] }],
+    password: [
+      '',
+      {
+        validators: [
+          Validators.required,
+          Validators.pattern(
+            '^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,}$'
+          ),
+        ],
+      },
+    ],
+  });
+
+  onSubmit(): void {
+    this.http
+      .post<{ user: UserInterface }>(
+        'https://api.realworld.io/api/users/login',
+        {
+          user: this.form.getRawValue(),
+        }
+      )
+      .subscribe((response) => {
+        console.log('response', response);
+        localStorage.setItem('token', response.user.token);
+        this.authService.currentUserSig.set(response.user);
+        this.router.navigateByUrl('/');
+      });
+  }
+}
